27f6346dae4692f437d5876767d493a1e0edfb29
kkhadka31 <kkhadka31@gmail.com>
2024-04-11 23:07:57 -0700

    testing github action build

--- src/test/java/com/koshish/KoKo/KoKoApplicationTests.java
+++ src/test/java/com/koshish/KoKo/KoKoApplicationTests.java
@@ -2,12 +2,8 @@ package com.koshish.KoKo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
-})
 class KoKoApplicationTests {
 
 	@Test
