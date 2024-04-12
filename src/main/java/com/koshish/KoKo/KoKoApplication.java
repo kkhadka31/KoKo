0b3e14b5999e0e8208474ddc7917336cedc82513
kkhadka31 <kkhadka31@gmail.com>
2024-04-11 23:16:47 -0700

    run ci

--- src/main/java/com/koshish/KoKo/KoKoApplication.java
+++ src/main/java/com/koshish/KoKo/KoKoApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }, scanBasePackages = {"com.koshish.KoKo",
-		"com.koshish.RestController", "com.koshish.Service", "com.koshish.Security"})
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }, scanBasePackages = {"com.koshish.KoKo", "com.koshish.RestController", "com.koshish.Service", "com.koshish.Security"})
 public class KoKoApplication {
 
 	public static void main(String[] args) {
