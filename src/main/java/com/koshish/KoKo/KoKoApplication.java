67eeafbda029b64f0cde88e5143da4d7e587688e
kkhadka31 <kkhadka31@gmail.com>
2024-04-11 22:59:55 -0700

    testing github action build

--- src/main/java/com/koshish/KoKo/KoKoApplication.java
+++ src/main/java/com/koshish/KoKo/KoKoApplication.java
@@ -3,8 +3,12 @@ package com.koshish.KoKo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }, scanBasePackages = {"com.koshish.KoKo", "com.koshish.RestController", "com.koshish.Service", "com.koshish.Security"})
+@ComponentScan(basePackages = {"com.koshish.KoKo",
+		"com.koshish.RestController", "com.koshish.Service", "com.koshish.Security"})
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }, scanBasePackages = {"com.koshish.KoKo",
+		"com.koshish.RestController", "com.koshish.Service", "com.koshish.Security"})
 public class KoKoApplication {
 
 	public static void main(String[] args) {
